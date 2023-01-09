21eb015039d4e8a3c899e5d803da76ced612fc24
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-01-09 21:06:17 +0100

    Finished component - Veggie

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import styled from "styled-components";
 import {Splide,SplideSlide} from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/splide.min.css';
+import { Card, Gradient, Wrapper } from '../styled_components/MyList.styled';
 
 function Popular() {
 
@@ -62,50 +62,7 @@ function Popular() {
   )
 }
 
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-`;
 
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
 
-  img{
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-  }
-
-  p{
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    widht: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-  }
-`;
-
-const Gradient = styled.div`
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0,5));
-`
 
 export default Popular
