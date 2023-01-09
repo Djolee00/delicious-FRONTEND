4352974cba727933171a383740f0560a61e82cc7
Djordje Ivanovic <ivanovic.djordje2019@gmail.com>
2023-01-09 19:48:44 +0100

    Finished fetching popular recipes

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import styled from "styled-components";
 
 function Popular() {
+
+  const [popular,setPopular] = useState([]);
+  
+  // with useEffect Hook we perform side effect in our component, after component is rendered
+  // [] - “only run on mount, and clean up on unmount”.
+  // mount - put element into DOM   
+  useEffect(() =>{
+    getPopular(); 
+  },[]);  
+
+  const getPopular = async () => {
+    const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
+    const data = await api.json();
+    setPopular(data.recipes);
+    console.log(data.recipes);
+  };
+
   return (
-    <div>Popular</div>
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) =>{
+              return(
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title}/>
+                </Card>
+              );
+            })}
+          </Wrapper>
+        )
+      })}
+    </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin:4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+`
 
 export default Popular
